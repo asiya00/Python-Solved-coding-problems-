1c5fe5845dbeffb984d6c9203f91b2f257db9f28
asiya00 <asiyapathan2000@gmail.com>
2021-08-18 22:18:18 +0530

    linked list

--- linkedlist.py
+++ linkedlist.py
@@ -1,114 +1,122 @@
-class node:
-    def __init__(self,data,next=None):
-        self.data = data
-        self.next = next
+class Node:
+	def __init__(self,data, next=None):
+		self.data = data
+		self.next = None
 
-class linkedlist:
-    def __init__(self):
-        self.head = None
+class LinkedList:
+	def __init__(self):
+		self.h = None
 
-    def insert_at_beginning(self, data):
-        self.head = node(data,self.head)
+	def insert_at_first(self, data):
+		self.h = Node(data, self.h)
 
-    def insert_at_end(self, data):
-        if not self.head:
-            self.insert_at_beginning(data)
-            return
-        temp = self.head
-        while(temp.next):
-            temp = temp.next
-        temp.next = node(data)
+	def insert_at_end(self, data):
+		if not self.h:
+			self.insert_at_first(data)
+		else:
+			temp = self.h 
+			while(temp.next):
+				temp = temp.next
 
-    def get_length(self):
-        counter = 0
-        temp = self.head
-        while(temp):
-            counter += 1
-            temp = temp.next
-        print("Total Length: ",counter)
-        return counter
+			temp.next = Node(data, next)
 
-    def insert_at_any_location(self,index, data):
-        if not self.head or index==0:
-            self.insert_at_beginning(data)
-            return
+	def get_length(self):
+		temp = self.h
+		counter = 0
+		while temp:
+			counter += 1
+			temp = temp.next
+		return counter
 
-        if index<0:
-            if abs(index) <= self.get_length():
-                index = (self.get_length() - abs(index)) + 1
-            else:
-                self.insert_at_beginning(data)
-                return
-        if index<=self.get_length():
-            count = 1
-            temp = self.head
-            while temp:
-                if count == index:
-                    temp.next = node(data,temp.next)
-                    return
-                count +=1
-                temp = temp.next
-        else:
-            self.insert_at_end(data)
-            return
+	def insert_at_index(self, index, data):
+		if not self.h or index == 0 or index == 1:
+			self.insert_at_first(data)
+			return
 
-    def remove_at_begining(self):
-        if not self.head:
-            print("linked list is empty!!!")
-            return
-        self.head = self.head.next
+		if index<0:
+			if abs(index) <= self.get_length():
+				index = self.get_length() + index + 1
+			else:
+				self.insert_at_first(data)
+				return
 
-    def remove_at_end(self):
-        if not self.head:
-            print("linked list is empty!!!")
-            return
-        if not self.head.next:
-            self.head = None
-            return
-        temp = self.head
-        while temp.next.next:
-            temp = temp.next
-        temp.next = None
+		if index < self.get_length():
+			count = 1
+			temp = self.h
+			while temp:
+				if count == index:
+					p = Node(data, next)
+					p.next = temp.next
+					temp.next = p
+					return
+				count +=1
+				temp = temp.next
+		else:
+			self.insert_at_end(data)
+			return
 
-    def remove_at_any_location(self,index):
-        if not self.head:
-            print("linked list is empty!!!")
-            return
-        if index > 1 and index <= self.get_length():
-            count = 1
-            temp = self.head
-            while temp:
-                if count == index-1:
-                    next = temp.next.next
-                    #temp.next = None
-                    temp.next = next
-                    return
-                temp = temp.next
-                count += 1
-        elif index == 1:
-            self.remove_at_begining()
-            return
-        else:
-            print("index not found")
-            return
+	def remove_end(self):
+		if not self.h:
+			print("Empty Linked List")
+			return 
+		temp = self.h
+		while temp.next.next:
+			temp = temp.next
 
-    def display(self):
-        temp = self.head
-        while temp:
-            print(temp.data," -> ", end = "")
-            temp = temp.next
-        print()
-        return
+		temp.next = None
 
-ll = linkedlist()
-ll.insert_at_beginning(15)
-ll.insert_at_beginning(10)
-ll.insert_at_end(30)
-ll.insert_at_any_location(-100, 50)
-ll.display()
-ll.remove_at_end()
-ll.display()
-ll.remove_at_begining()
-ll.display()
-ll.remove_at_any_location(2)
-ll.display()
+	def remove_at_first(self):
+		if not self.h:
+			print("Linked List Empty")
+			return
+		self.h = self.h.next
+
+	def remove_at_index(self, index):
+		if not self.h:
+			print("Linked List is empty")
+			return
+		if index==1:
+			self.remove_at_first()
+		if index <= self.get_length():
+			count = 1
+			temp = self.h
+			while temp:
+				if count == index:
+					temp.next = temp.next.next
+					return
+				count += 1
+				temp = temp.next
+
+
+	def display(self):
+		temp = self.h 
+		while(temp):
+			print(temp.data,end=" ")
+			temp = temp.next
+		print()
+
+
+li = LinkedList()
+
+li.insert_at_first(10)
+li.insert_at_end(20)
+li.insert_at_end(50)
+li.insert_at_end(60)
+li.insert_at_end(70)
+li.insert_at_end(80)
+li.insert_at_end(15)
+li.display()
+li.insert_at_index(2,30)
+li.display()
+li.insert_at_index(-2,40)
+li.display()
+li.display()
+li.remove_end()
+li.display()
+li.remove_at_first()
+li.display()
+li.remove_at_index(3)
+li.display()
+
+
+
