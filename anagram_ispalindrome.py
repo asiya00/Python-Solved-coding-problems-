7968c65ae28ddb75e6d07d9da733d4a1a044d03c
asiya00 <asiyapathan2000@gmail.com>
2021-11-25 11:05:34 +0530

    code

---
+++ anagram_ispalindrome.py
@@ -0,0 +1,19 @@
+s = "geeksforgeeks"
+
+def check_anagram_palindrome(s):
+	count = [0 for i in range(256)]
+
+	for x in s:
+		count[ord(x)] += 1
+
+	odd_count = 0 
+	for x in s:
+		if count[ord(x)] & 1:
+			odd_count += 1
+		if odd_count > 1:
+			return "False"
+			break
+	return "True"
+
+print(check_anagram_palindrome(s))
+	
