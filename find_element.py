f3f43144bff16a55014edea323be1b881197734f
asiya00 <asiyapathan2000@gmail.com>
2021-12-17 18:31:02 +0530

    code

--- find_element.py
+++ find_element.py
@@ -2,32 +2,33 @@ def findElement(arr, n):
     left = 0
     i = 1
     right = i+1
-    
-    while i < n:
-        while i < n and left > -1:
-            if arr[left] > arr[i]:
-                break
-            else:
+    while i < n - 1:
+        while i < n - 1 and left > -1:
+            if arr[left] <= arr[i]:
                 left -= 1
-        print(i, right)
-        while i < n and right < n:
-            # print(arr[right], arr[i])
-            if arr[right] < arr[i]:
-                break
             else:
-                right += 1
-        # print(left, right)
-        if i < n and left == 0 and right == n - 1 :
-            return arr[i]
-        else:
-            i += 1
-            left = i - 1
-            right = i + 1
-    else:
-        return -1
+                break
+        if left == -1:
+            left += 1
+            while i < n - 1 and right < n:
+                if arr[right] >= arr[i]:
+                    right += 1
+                else:
+                    break
 
-# n = 14
-# arr = [5, 6, 4, 1, 7, 12, 9, 1, 4, 1, 11, 5, 7, 1]
-n = 5
-arr = [4, 2, 5, 7]
+            if right == n:
+                right -= 1
+
+            if i < n and arr[left] <= arr[i] and arr[right] >= arr[i]:
+                return arr[i]
+        i += 1
+        left = i - 1
+        right = i + 1
+    return -1
+
+
+n = 14
+arr = [5, 6, 4, 1, 7, 12, 9, 1, 4, 1, 11, 5, 7, 1]
+# n = 4
+# arr = [4, 2, 5, 7]
 print(findElement(arr, n))
