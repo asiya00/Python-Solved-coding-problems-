a22b604b77c336020175a41bd99828a1cd9fd11c
asiya00 <asiyapathan2000@gmail.com>
2022-01-19 00:42:08 +0530

    code

--- generate_balance_string.py
+++ generate_balance_string.py
@@ -1,20 +1,18 @@
-def count_balanced_parenthesis(s, l, r):
+li = []
+def generate_balanced_parenthesis(s, l, r):
     if not l and not r:
-        return s
+        li.append(s)
+        return ""
     if l > r:
         return ""
     if not r:
-        return count_balanced_parenthesis(s+"(", l-1, r)
+        return generate_balanced_parenthesis(s+"(", l-1, r)
     if not l:
-        return count_balanced_parenthesis(s+")", l, r-1)
-    return count_balanced_parenthesis(s+"(", l-1, r) + count_balanced_parenthesis(s+")", l, r-1)
+        return generate_balanced_parenthesis(s+")", l, r-1)
+    return generate_balanced_parenthesis(s+"(", l-1, r) + generate_balanced_parenthesis(s+")", l, r-1)
 
 
-n = 4
+n = 3
 s = ""
-a = count_balanced_parenthesis(s, n, n)
-count = 0
-for i in range(0, len(a), n*2):
-    count += 1
-    print(a[i:i+n*2])
-print(count)
+generate_balanced_parenthesis(s, n, n)
+print(li)
