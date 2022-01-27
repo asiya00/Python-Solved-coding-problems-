cf2f25cbac3bcb457bb54da44a999ab7950bd264
asiya00 <asiyapathan2000@gmail.com>
2022-01-28 01:41:57 +0530

    code

--- bst.py
+++ bst.py
@@ -68,7 +68,7 @@ class BST:
         if not self.rchild:
             return self.key
         else:
-            return self.rchild.getmin_ele()
+            return self.rchild.getmax_ele()
 
 
     # Delete method
@@ -142,6 +142,52 @@ class BST:
             self.rchild.postorder()
         print(self.key, end=" ")
 
+    # 4. Level order Traversal
+    def levelorder(self, queue=[]):
+        if not self:
+            return []
+        queue = [self]
+        ans = []
+        while queue:
+            li = []
+            ans.append([node.key for node in queue])
+            for node in queue:
+                if node.lchild:
+                    li.append(node.lchild)
+                if node.rchild:
+                    li.append(node.rchild)
+            queue = li
+        print(ans)
+        
+    
+    #5. Vertical order traversal
+    def verticalorder(self):
+        queue = [self]
+        node_di = {}
+        di = {}
+        di[self.key] = 0
+        node_di[0] = [self.key]
+        while queue:
+            curr = queue.pop(0)
+            if curr.lchild:
+                di[curr.lchild.key] = di[curr.key] - 1
+                if di[curr.lchild.key] in node_di:
+                    node_di[di[curr.lchild.key]].append(curr.lchild.key)
+                else:
+                    node_di[di[curr.lchild.key]] = [curr.lchild.key]
+                queue.append(curr.lchild)
+            if curr.rchild:
+                di[curr.rchild.key] = di[curr.key] + 1
+                if di[curr.rchild.key] in node_di:
+                    node_di[di[curr.rchild.key]].append(curr.rchild.key)
+                else:
+                    node_di[di[curr.rchild.key]] = [curr.rchild.key]
+                queue.append(curr.rchild)
+        print(di)
+        ans = sorted(node_di.items(), key=lambda x:(x[0], x[1]))
+        print(ans)
+
+
 def count(node):
     if node is None:
         return 0
@@ -151,7 +197,7 @@ tree = BST(None)
 
 # tree.insert(20)
 
-for i in [10, 7, 8, 9, 11, 2]:
+for i in [8, 3, 10, 1, 6, 9, 14]:
     tree.insert(i)
 
 print("Preorder Traversal")
@@ -166,10 +212,13 @@ tree.postorder()
 print()
 tree.search(10)
 
-if count(tree) > 1:
-    tree.delete(10, tree.key)
-else:
-    print("Delete operation can't be performed")
+# if count(tree) > 1:
+#     tree.delete(10, tree.key)
+# else:
+#     print("Delete operation can't be performed")
 tree.preorder()
 print("\nMinimum Element",tree.getmin_ele())
 print("\nMaximum Element",tree.getmax_ele())
+print("Level order Traversal")
+tree.levelorder()
+tree.verticalorder()
